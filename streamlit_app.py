51c64edbcd4a6ea947c409a5cb8ceacfa6d85a1d
prayuktich <121225906+prayuktich@users.noreply.github.com>
2022-12-30 16:38:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,33 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('My Moms New Healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.title('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#streamlit.dataframe(my_fruit_list)
+
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Let's put a pick list here so they can pick the fruit they want to include 
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered',fruit_choice)
@@ -25,8 +35,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
-
-import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
